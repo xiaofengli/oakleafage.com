71486d2dead69bf8a2ae042d8bb4012336d151e6
TonyDeng1997 <denghanfei1997@gmail.com>
2018-03-13 23:59:45 -0500

    check IP 50% done

--- app/app.js
+++ app/app.js
@@ -2,13 +2,48 @@
 var express = require('express');
 var reload = require('reload');
 var app = express();
+var geoip = require('geoip-lite');
 //var io = require('socket.io')();
+
+
+var requestIp = require('request-ip');
+
+// inside middleware handler
+
+var getClientIp = function(req) {
+	sadsadasdasd
+  var ipAddress = req.connection.remoteAddress;
+if (!ipAddress) {
+    return '';
+  }
+// convert from "::ffff:192.0.0.1"  to "192.0.0.1"
+  if (ipAddress.substr(0, 7) == "::ffff:") {
+    ipAddress = ipAddress.substr(7)
+  }
+return ipAddress;
+};
+
+app.use(function(req, res, next) {
+  var ipAddress = getClientIp(req);
+  console.log(ipAddress);
+});
+
+
+
 
 //Load data
 var dataFile = require('./data/data.json');
 var privacyStatement = require('./data/privacy.json');
 var termOfUse = require('./data/termofuse.json');
 
+
+/* Input IP and then output contry, that ip is just for checking purpose
+var geoip = require('geoip-lite');
+ 
+var ip = "207.97.227.239";
+var geo = geoip.lookup(ip);
+ 
+console.log(geo);
 /*i18n stuff*/
 var cookieParser = require('cookie-parser');
 app.use(cookieParser());
