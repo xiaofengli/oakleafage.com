d5729b9bf744c87b700b4b0d773bf99c3af4b3b5
TonyDeng1997 <denghanfei1997@gmail.com>
2018-03-22 14:57:45 -0500

    added image modal and form

--- app/routes/api.js
+++ app/routes/api.js
@@ -2,26 +2,35 @@ var express = require('express');
 var router = express.Router();
 var bodyParser = require('body-parser');
 var fs = require('fs');
-var feedbackData = require('../data/feedback.json');
-
+//var feedbackData = require('../data/feedback.json');
+/*
 router.get('/api', function(req, res) {
   res.json(feedbackData);
 });
+*/
 
 router.use(bodyParser.json());
 router.use(bodyParser.urlencoded({ extended: false }));
 
 router.post('/api', function(req, res) {
+  var feedbackData=[];
   feedbackData.unshift(req.body);
-  fs.writeFile('app/data/feedback.json', JSON.stringify(feedbackData), 'utf8', function(err) {
+  console.log(req.file);
+
+  fs.writeFile('app/data/feedbacks/'+feedbackData[0].firstname+Date()+'.json', JSON.stringify(feedbackData), 'utf8', function(err) {
     if (err) {
       console.log(err);
     }
   });
-  res.json(feedbackData);
+  fs.writeFile('app/data/feedbacks/'+feedbackData[0].firstname+Date()+'.PNG', req.file , 'utf8', function(err) {
+    if (err) {
+      console.log(err);
+    }
+  });
+  //res.json(feedbackData);
 });
 
-
+/*
 router.delete('/api/:id', function(req, res) {
   feedbackData.splice(req.params.id, 1);
   fs.writeFile('app/data/feedback.json', JSON.stringify(feedbackData), 'utf8', function(err) {
@@ -32,6 +41,6 @@ router.delete('/api/:id', function(req, res) {
   res.json(feedbackData);
 });
 
-
+*/
 
 module.exports = router;
