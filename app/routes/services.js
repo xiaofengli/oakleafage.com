b12349891fe07d98b5b35a0195f6c8cf9b971f14
HanfeiDeng <TonyDeng@HanfeiDengs-MacBook-Pro.local>
2018-06-07 13:21:09 -0500

    dropdown bar form verification

--- app/routes/services.js
+++ app/routes/services.js
@@ -5,7 +5,7 @@ router.get('/services', (req, res) => {
   var data = req.app.get('appData');
   var pagePhotos = [];
   var pageServices = req.app.get('i18n') === 'CN' ? data.services.cn : data.services.en;
-  //console.log(req.app.get('i18n'));
+  console.log(req.app.get('i18n'));
   pageServices.forEach(function(item) {
     pagePhotos = pagePhotos.concat(item.image);
   });
@@ -23,7 +23,7 @@ router.get('/services/:serviceid', (req, res) => {
   var pagePhotos = [];
   var pageServices = [];
   var services = req.app.get('i18n') === 'CN' ? data.services.cn : data.services.en;
-  //console.log(req.app.get('i18n'));
+  console.log(req.app.get('i18n'));
    services.forEach(function(item) {
     if (item.shortname == req.params.serviceid) {
     	pageServices.push(item);
